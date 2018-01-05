cc0de887d52a1de963cb74cdce5f6d87467b5482
agatan <agatan039@gmail.com>
2018-01-06 00:02:33 +0900

    Remove unused functions

--- src/states/TodoState.ts
+++ src/states/TodoState.ts
@@ -5,20 +5,3 @@ export interface Todo {
 }
 
 export type Todos = Todo[];
-
-let id = 0;
-
-function generateTodo(text: string, id: number): Todo {
-    return {
-        id: id,
-        completed: false,
-        text: text,
-    };
-}
-
-export function generateTodos(texts: string[]): Todos {
-    return texts.map(text => {
-        id++;
-        return generateTodo(text, id-1);
-    });
-}
