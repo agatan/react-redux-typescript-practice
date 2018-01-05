cb4c97eafeeeef74910dc1a95d215ba5ec1744be
agatan <agatan039@gmail.com>
2018-01-05 19:34:30 +0900

    Define actions

---
+++ src/actions/index.ts
@@ -0,0 +1,36 @@
+import { Action } from 'redux';
+
+export enum TodoActionType {
+    ADD_TODO = 'todo/add',
+    TOGGLE_TODO = 'todo/toggle',
+}
+
+export interface AddTodoAction extends Action {
+    type: TodoActionType.ADD_TODO;
+    id: number;
+    text: string;
+}
+
+export interface ToggleTodoAction extends Action {
+    type: TodoActionType.TOGGLE_TODO;
+    id: number;
+}
+
+export type TodoAction = AddTodoAction | ToggleTodoAction;
+
+let nextTodoId = 0;
+
+export function addTodo(text: string): AddTodoAction {
+    return {
+        type: TodoActionType.ADD_TODO,
+        id: nextTodoId++,
+        text: text,
+    };
+}
+
+export function toggleTodo(id: number): ToggleTodoAction {
+    return {
+        type: TodoActionType.TOGGLE_TODO,
+        id: id,
+    };
+}
