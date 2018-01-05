7e4637a4c97df134a83a847d60e87f883e765879
agatan <agatan039@gmail.com>
2018-01-06 00:57:28 +0900

    Add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import TodoList from './containers/TodoList';
 import AddTodoButton from './containers/AddTodoButton';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
     <div>
       <AddTodoButton />
       <TodoList />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import FilterLink from '../containers/FilterLink';
+import { VisibilityFilter } from '../states/VisibilityState';
+
+const Footer = () => (
+  <p>
+    Show: {' '}
+    <FilterLink filter={VisibilityFilter.ShowAll}>
+      All
+    </FilterLink>
+    {', '}
+    <FilterLink filter={VisibilityFilter.ShowActive}>
+      Active
+    </FilterLink>
+    {', '}
+    <FilterLink filter={VisibilityFilter.ShowCompleted}>
+      Completed
+    </FilterLink>
+  </p>
+);
+
+export default Footer;
