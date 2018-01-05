213799a0c77278e35f56ef290ccc247e463ca831
agatan <agatan039@gmail.com>
2018-01-05 19:01:13 +0900

    Define TodoList

---
+++ src/components/TodoList.test.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import * as enzyme from 'enzyme';
+import Todo from './Todo';
+import TodoList from './TodoList';
+
+import * as Adapter from 'enzyme-adapter-react-16';
+
+enzyme.configure({ adapter: new Adapter() });
+
+it("renders text when completed=true", () => {
+    const dummyCb = (id: number) => { return; };
+    const todos = [
+        { id: 0, text: "Hello", completed: true },
+        { id: 1, text: "World", completed: true },
+    ];
+    const hello = enzyme.shallow(
+        <TodoList
+            todos={todos}
+            onTodoClick={dummyCb}/>
+    );
+    expect(hello.find(Todo)).toHaveLength(2);
+});
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Todo from './Todo';
+import * as State from '../states/TodoState';
+
+export interface TodoListProps {
+    todos: State.Todos;
+    onTodoClick: (id: number) => void;
+}
+
+export default class TodoList extends React.PureComponent<TodoListProps, {}> {
+    render() {
+        const { todos, onTodoClick } = this.props;
+        return (
+            <ul>
+                {
+                    todos.map(todo =>
+                        <Todo
+                            key={todo.id}
+                            {...todo}
+                            onClick={() => onTodoClick(todo.id)}
+                        />
+                    )
+                }
+            </ul>
+        );
+    }
+}
