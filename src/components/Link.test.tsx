3ac44cf164d6d92222bd9bd3a2acd83cae114121
agatan <agatan039@gmail.com>
2018-01-06 00:10:59 +0900

    Define Link

---
+++ src/components/Link.test.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import * as enzyme from 'enzyme';
+import Link from './Link';
+
+import * as Adapter from 'enzyme-adapter-react-16';
+
+enzyme.configure({ adapter: new Adapter() });
+
+it("renders link as <a/> if not active", () => {
+    const dummyCb = () => { return; };
+    const wrapper = enzyme.shallow(
+        <Link active={false} onClick={dummyCb}>
+            Hello
+        </Link>
+    );
+
+    expect(wrapper.find("a").text()).toEqual("Hello");
+});
+
+it("renders link as <span/> if active", () => {
+    const dummyCb = () => { return; };
+    const wrapper = enzyme.shallow(
+        <Link active={true} onClick={dummyCb}>
+            Hello
+        </Link>
+    );
+
+    expect(wrapper.find("span").text()).toEqual("Hello");
+});
