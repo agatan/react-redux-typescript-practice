d85d7fd8f80b89e33be7060b21dd7906b3d11f7d
agatan <agatan039@gmail.com>
2018-01-05 19:28:24 +0900

    Define AddTodoButton

---
+++ src/components/AddTodoButton.test.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import * as enzyme from 'enzyme';
+import AddTodoButton from './AddTodoButton';
+
+import * as Adapter from 'enzyme-adapter-react-16';
+
+enzyme.configure({ adapter: new Adapter() });
+
+it("submit input value", () => {
+    const dummyCb = (s: string) => { return; };
+    const wrapper = enzyme.shallow(
+        <AddTodoButton onSubmit={dummyCb}/>
+    );
+    wrapper.setState({ input: "Hello" });
+    expect(wrapper.find('input').prop('value')).toBe("Hello");
+});
